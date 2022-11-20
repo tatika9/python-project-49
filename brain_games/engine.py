06ff9b82e362b2a49550a44ee668767e249eab6e
Rinat Shakirov <tatika0872@gmail.com>
2022-11-20 10:47:08 +0500

    Fix brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -12,8 +12,8 @@ def dialogue(rules, question_answer):
         if answer == correct_answer:
             print('Correct!')
         else:
-            print((f"'{answer}' is wrong answer ;(. "
-                  "Correct answer was '{correct_answer}'"))
+            print(f"'{answer}' is wrong answer ;(. "
+                  f"Correct answer was '{correct_answer}'")
             print(f"Let's try again, {name}!")
             return
     print(f'Congratulations, {name}')
