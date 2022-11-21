b13ae7622cd9154cb704d19ea6f86575c1352f02
Rinat Shakirov <tatika0872@gmail.com>
2022-11-21 20:34:46 +0500

    Refactor: function is_even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,6 +1,13 @@
 import random
 
 
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
 def generation():
     RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
     NUMBER_MIN = 1
@@ -9,7 +16,7 @@ def generation():
     question_answer = []
     for _ in range(ROUND_COUNT):
         number = random.randint(NUMBER_MIN, NUMBER_MAX)
-        if number % 2 == 0:
+        if is_even(number):
             correct_answer = 'yes'
         else:
             correct_answer = 'no'
